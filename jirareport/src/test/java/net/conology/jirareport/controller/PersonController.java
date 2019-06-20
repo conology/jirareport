a560a92caf0fa328300aa92659f9997160edf57f
Willm <willm.tueting@conology.net>
2019-06-20 17:21:55 +0200

    gui

---
+++ jirareport/src/test/java/net/conology/jirareport/controller/PersonController.java
@@ -0,0 +1,54 @@
+package net.conology.jirareport.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.conology.jirareport.model.*;
+
+import net.conology.jirareport.model.Person;
+
+@Controller
+@RequestMapping("/persons")
+public class PersonController {
+
+	// load person data
+	
+	private List<Person> thePersons;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		// create persons
+		Person per1 = new Person(1, "Aleš",40,0);
+		Person per2 = new Person(2, "Rok",35,5);
+		Person per3 = new Person(3, "Bojan",20,20);
+		
+
+		// create the list
+		thePersons = new ArrayList<>();
+		
+		// add to the list
+		thePersons.add(per1);
+		thePersons.add(per2);
+		thePersons.add(per3);
+	
+	}
+	
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listPersons(Model theModel) {
+		
+		// add to the spring model
+		theModel.addAttribute("persons", thePersons);
+		
+		return "display";
+	}
+}
---
+++ jirareport/src/test/java/net/conology/jirareport/model/Person.java
@@ -0,0 +1,48 @@
+package net.conology.jirareport.model;
+public class Person {
+	
+private int id;
+private String name;
+private int sumOfTotalHours;
+private int remainingHours;
+
+public Person() {
+	
+}
+public Person(int id, String name, int sumOfTotalHours, int remainingHours) {
+	this.id = id;
+	this.name = name;
+	this.sumOfTotalHours = sumOfTotalHours;
+	this.remainingHours = remainingHours;
+			
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getSumOfTotalHours() {
+	return sumOfTotalHours;
+}
+public void setSumOfTotalHours(int sumOfTotalHours) {
+	this.sumOfTotalHours = sumOfTotalHours;
+}
+public int getRemainingHours() {
+	return remainingHours;
+}
+public void setRemainingHours(int remainingHours) {
+	this.remainingHours = remainingHours;
+}
+
+@Override
+public String toString() {
+	return "Person [id=" + id + ", name=" + name + ", sumOfTotalHours=" + sumOfTotalHours + ", remainingHours=" + remainingHours + "]";
+}
+}
