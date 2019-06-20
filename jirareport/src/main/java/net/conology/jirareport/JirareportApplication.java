812967217039dbadd58dbb115c413b8984fd514d
Willm <willm.tueting@conology.net>
2019-06-20 17:13:56 +0200

    initial commit

---
+++ jirareport/src/main/java/net/conology/jirareport/JirareportApplication.java
@@ -0,0 +1,13 @@
+package net.conology.jirareport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JirareportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JirareportApplication.class, args);
+	}
+
+}
