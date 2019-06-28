3dca9890f1d2bbd176a13a4a7978fed796fe4fa3
noone-really <38106255+noone-really@users.noreply.github.com>
2019-06-28 15:43:57 +0200

    changed some things and pushing

--- jirareport/src/main/java/net/conology/jirareport/model/jira/JiraConnector.java
+++ jirareport/src/main/java/net/conology/jirareport/model/jira/JiraConnector.java
@@ -9,76 +9,76 @@ import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientF
 import com.atlassian.jira.rest.client.api.domain.Issue;
 
 public class JiraConnector {
-	
+
 	private String username;
 	private String password;
 	private String jiraUrl;
 	private JiraRestClient restClient;
-	
-	 public static void main(String[] args) {
-		//JiraConnector con = new JiraConnector("willm.tueting@conology.net","TestingTesting","http://conology.atlassian.net");
-		 JiraConnector con = new JiraConnector("willm.tueting@conology.net","m628BRhnEj5wDvDFi9cXDE59","http://conology.atlassian.net");
-		 //JiraConnector con = new JiraConnector("rok.pusnik@conology.onmicrosoft.com","nZvnprWZ234FK4RjIDWk32E6","http://conology.atlassian.net");
-		 //con.getIssuesOfSprint();
-		 con.transformToSprint(con.getIssuesOfSprint());
-	 }
-	
-	
+
+	public static void main(String[] args) {
+		// JiraConnector con = new
+		// JiraConnector("willm.tueting@conology.net","TestingTesting","http://conology.atlassian.net");
+		JiraConnector con = new JiraConnector("willm.tueting@conology.net", "m628BRhnEj5wDvDFi9cXDE59",
+				"http://conology.atlassian.net");
+		// JiraConnector con = new
+		// JiraConnector("rok.pusnik@conology.onmicrosoft.com","nZvnprWZ234FK4RjIDWk32E6","http://conology.atlassian.net");
+		// con.getIssuesOfSprint();
+		con.transformToSprint(con.getIssuesOfSprint());
+	}
+
 	public JiraConnector(String username, String password, String jiraUrl) {
-	    this.username = username;
-	    this.password = password;
-	    this.jiraUrl = jiraUrl;
-	    this.restClient = getJiraRestClient();
+		this.username = username;
+		this.password = password;
+		this.jiraUrl = jiraUrl;
+		this.restClient = getJiraRestClient();
 	}
-	
+
 	private JiraRestClient getJiraRestClient() {
-	    return new AsynchronousJiraRestClientFactory()
-	      .createWithBasicHttpAuthentication(getJiraUri(), this.username, this.password);
+		return new AsynchronousJiraRestClientFactory().createWithBasicHttpAuthentication(getJiraUri(), this.username,
+				this.password);
 	}
-	
-/*	private JiraRestClient getJiraRestClient2() {
-	   
-		AsynchronousJiraRestClientFactory async = new AsynchronousJiraRestClientFactory();
-	    AuthenticationHandler handle = new AuthenticationHandler();
-		
-		return new AsynchronousJiraRestClientFactory()
-	      .createWithBasicHttpAuthentication(getJiraUri(), this.username, this.password);
-	}*/
-	
-	
-	
-	
+
+	/*
+	 * private JiraRestClient getJiraRestClient2() {
+	 * 
+	 * AsynchronousJiraRestClientFactory async = new
+	 * AsynchronousJiraRestClientFactory(); AuthenticationHandler handle = new
+	 * AuthenticationHandler();
+	 * 
+	 * return new AsynchronousJiraRestClientFactory()
+	 * .createWithBasicHttpAuthentication(getJiraUri(), this.username,
+	 * this.password); }
+	 */
+
 	private SearchResult getIssuesOfSprint() {
-		//return restClient.getSearchClient().searchJql("Sprint in openSprints() AND Sprint not in futureSprints()").claim();
+		// return restClient.getSearchClient().searchJql("Sprint in openSprints() AND
+		// Sprint not in futureSprints()").claim();
 		return restClient.getSearchClient().searchJql("Status not in (done) AND assignee = rok.pusnik").claim();
 	}
-	
-	private void transformToSprint (SearchResult result) {
-		
+
+	private void transformToSprint(SearchResult result) {
+
 		Sprint currentSprint = new Sprint();
-	
-		for (Issue issue: result.getIssues()) {
+
+		for (Issue issue : result.getIssues()) {
 			SimplifiedIssue currentIssue = new SimplifiedIssue();
-			
-			if( issue.getTimeTracking() != null ) {
-			currentIssue.timeEstimate = issue.getTimeTracking().getOriginalEstimateMinutes();
-			currentIssue.timeLeft = issue.getTimeTracking().getRemainingEstimateMinutes();
-			currentIssue.timeSpent = issue.getTimeTracking().getTimeSpentMinutes();}
+
+			if (issue.getTimeTracking() != null) {
+				currentIssue.timeEstimate = issue.getTimeTracking().getOriginalEstimateMinutes();
+				currentIssue.timeLeft = issue.getTimeTracking().getRemainingEstimateMinutes();
+				currentIssue.timeSpent = issue.getTimeTracking().getTimeSpentMinutes();
+			}
 			currentIssue.id = issue.getId().toString();
 			currentIssue.key = issue.getKey();
 			currentIssue.description = issue.getDescription();
-			
-			
-			currentSprint.addIssue(currentIssue);
+			System.out.println(currentIssue.key);
+			//currentSprint.addIssue(currentIssue);
 		}
-	
-		
-	}
-	
 
-	
+	}
+
 	private URI getJiraUri() {
-	    return URI.create(this.jiraUrl);
+		return URI.create(this.jiraUrl);
 	}
 
 }
